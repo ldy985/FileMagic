a74b6225f004c261d655dd00ab6fa4d9d5c9c37c
Lasse <lasse@dyrsted.dk>
2021-01-26 00:19:44 +0100

    Added TGA & TTF

---
+++ src/FileMagic.Core.Rules/Rules/TGARule.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+using ldy985.BinaryReaderExtensions;
+using ldy985.FileMagic.Abstracts;
+using Microsoft.Extensions.Logging;
+
+namespace ldy985.FileMagic.Core.Rules.Rules
+{
+    /// <summary>
+    /// http://www.paulbourke.net/dataformats/tga/
+    /// https://www.fileformat.info/format/tga/egff.htm
+    /// http://www.opennet.ru/docs/formats/targa.pdf
+    /// https://www.dca.fee.unicamp.br/~martino/disciplinas/ea978/tgaffs.pdf
+    /// </summary>
+    public class TGARule : BaseRule
+    {
+        public override IMagic? Magic { get; }
+
+        /// <inheritdoc />
+        public override ITypeInfo TypeInfo { get; } = new TypeInfo("TGA image file", "TGA");
+
+        /// <inheritdoc />
+        public TGARule(ILogger<TGARule> logger) : base(logger)
+        {
+        }
+
+        protected override bool TryStructureInternal([NotNull] BinaryReader reader, IResult result)
+        {
+            var position = reader.GetPosition();
+            reader.SetPosition(reader.GetLength() - 18);
+
+            //Check for v2 optional footer
+            var readFixedString = reader.ReadFixedString(17, Encoding.ASCII);
+            if (readFixedString == "TRUEVISION-XFILE.")
+                return true;
+
+            reader.SetPosition(position + 2);
+            var colorMap = reader.ReadByte();
+            if (colorMap > 2)
+                return false;
+
+            var dataType = reader.ReadByte();
+            switch (dataType)
+            {
+                // case 0: //this is useless eg. empty image
+                case 1:
+                case 2:
+                case 3:
+                case 9:
+                case 10:
+                case 11:
+                case 32:
+                case 33:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
