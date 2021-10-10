a6c191c535ef123449615e5ad8c44cbccf54d240
Lasse D <lasse@dyrsted.dk>
2021-10-10 12:35:33 +0200

    Example

--- src/FileMagic.Examples/Program.cs
+++ src/FileMagic.Examples/Program.cs
@@ -1,12 +1,12 @@
 using System.Threading.Tasks;
-using ldy985.FileMagic;
-using ldy985.FileMagic.Abstracts;
 using ldy985.FileMagic.Core.Extensions;
 using ldy985.FileMagic.Core.Rules;
+using ldy985.FileMagic.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace ConsoleApp
+namespace ldy985.FileMagic.Examples
 {
     public class Program
     {
@@ -20,17 +20,25 @@ namespace ConsoleApp
             return Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    IFileMagicBuilder magicBuilder = services.AddFileMagicCore(config =>
+                    services.AddFileMagicCore(config =>
                         {
                             config.PatternCheck = true;
                             config.StructureCheck = true;
                         })
                         .UseFileMagic()
                         .AddDefaultFileMagicRules();
-                    IParsedHandlerProvider handlerProvider = magicBuilder.AddDefaultParsedHandler();
+                    // IParsedHandlerProvider handlerProvider = magicBuilder.AddDefaultParsedHandler();
 
 
                     services.AddHostedService<Worker>();
+                }).ConfigureLogging(builder =>
+                {
+                    builder.AddSimpleConsole(options =>
+                    {
+                        options.SingleLine = true;
+                        options.IncludeScopes = true;
+                        options.TimestampFormat = "hh:mm:ss ";
+                    });
                 });
         }
     }
