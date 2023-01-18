567da96edd57ef877e47f371368b2376690c2104
Lasse D <ldy@csis.com>
2023-01-18 13:06:28 +0100

    Update PNG rule

--- src/FileMagic.Core.Rules/Rules/Media/PNGRule.cs
+++ src/FileMagic.Core.Rules/Rules/Media/PNGRule.cs
@@ -3,13 +3,16 @@ using Microsoft.Extensions.Logging;
 
 namespace ldy985.FileMagic.Core.Rules.Rules.Media;
 
+/// <summary>
+/// https://www.w3.org/TR/2022/WD-png-3-20221025/#5PNG-file-signature
+/// </summary>
 public class PNGRule : BaseRule
 {
     /// <inheritdoc />
     public PNGRule(ILogger<PNGRule> logger) : base(logger) { }
 
     /// <inheritdoc />
-    public override IMagic Magic { get; } = new Magic("89504E470D0A1A0A0000000D49484452");
+    public override IMagic Magic { get; } = new Magic("89504E470D0A1A0A");
 
     /// <inheritdoc />
     public override Quality Quality => Quality.High;
